9af0385b0599ea92e656b5edec7779efb8f4778c
Jan Grzybek <jan@amperecomputing.com>
2025-10-21 14:15:41 +0200

    Update run.sh

--- benchmarks/run.sh
+++ benchmarks/run.sh
@@ -1,4 +1,4 @@
 set -e
 
-python3 run.py -m Meta-Llama-3-8B-Instruct.Q8_0.gguf -t 10 16 32 40 64 80 -b 1 2 4 8 16 32 64 -p 512 -r 0-79 -fa 1
+python3 run.py -m Meta-Llama-3-8B-Instruct.Q8_0.gguf -t 10 16 32 40 64 80 -b 1 2 4 8 16 32 64 -p 512 -r 0-79 -fa
 rm -f /tmp/log_power
